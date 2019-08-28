def7b8924a218f7f2acac3eb1f5474e515c1d46f
owen.le.goldenowl <owen.le.goldenowl@gmail.com>
2019-08-28 17:47:55 +0700

    make dropdown menu

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
     $(document).scroll(function () {
         scroll_pos = $(this).scrollTop();
         if (scroll_pos > 0) {
-            $('.menutren').css('background-color', '#292525cc');
+            $('.navbar').css('background-color', '#292525cc');
         } else if (scroll_pos == 0) {
-            $('.menutren').css('background-color', 'rgba(41, 37, 37, 0)');
+            $('.navbar').css('background-color', 'rgba(41, 37, 37, 0)');
         }
     });
 
