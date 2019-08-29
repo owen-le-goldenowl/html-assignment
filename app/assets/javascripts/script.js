93ed2a30ded8fff96a62c487838fcc690a76106c
owen.le.goldenowl <owen.le.goldenowl@gmail.com>
2019-08-29 09:42:44 +0700

    did the image preview

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // handle navbar background color change when start scrolling
     var scroll_pos = 0;
     $(document).scroll(function () {
         scroll_pos = $(this).scrollTop();
@@ -9,6 +10,7 @@ $(document).ready(function () {
         }
     });
 
+    //handle image drag effect
     $(".carousel").swipe({
         swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
             if (direction == 'left') $(this).carousel('next');
@@ -17,4 +19,22 @@ $(document).ready(function () {
         allowPageScroll: "vertical"
     });
 
+    // handle image preview
+    $('#image-preview #image-input').change(function (e) {
+        e.preventDefault();
+        if (this.files.length>0){
+           var imgFile=this.files[0];
+           var fileType=imgFile.type;
+           var validFileTypes=["image/gif","image/png","image/jpg","image/jpeg"];
+           if (validFileTypes.includes(fileType)){
+                var reader=new FileReader();
+                reader.readAsDataURL(imgFile);
+                reader.onload=function(){
+                    $('#image-show').attr('src',reader.result);
+                }
+           }else{
+               alert('Not an image!');
+           }
+        }
+    });
 });
