6ecfc1093ac0ec600c214b652b0e05cf9436758b
owen.le.goldenowl <owen.le.goldenowl@gmail.com>
2019-08-29 09:50:15 +0700

    Revert "make dropdown menu"
    This reverts commit def7b8924a218f7f2acac3eb1f5474e515c1d46f.

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -4,9 +4,9 @@ $(document).ready(function () {
     $(document).scroll(function () {
         scroll_pos = $(this).scrollTop();
         if (scroll_pos > 0) {
-            $('.navbar').css('background-color', '#292525cc');
+            $('.menutren').css('background-color', '#292525cc');
         } else if (scroll_pos == 0) {
-            $('.navbar').css('background-color', 'rgba(41, 37, 37, 0)');
+            $('.menutren').css('background-color', 'rgba(41, 37, 37, 0)');
         }
     });
 
